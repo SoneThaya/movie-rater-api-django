ca1997cf91b9ebfc533f8f0bff7b0af2da8c98d4
sone <sthaya@gmail.com>
2021-05-05 20:29:21 -0700

    update and create

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -14,10 +15,23 @@ class MovieViewSet(viewsets.ModelViewSet):
         if 'stars' in request.data:
             
             movie = Movie.objects.get(id=pk)
-            print('movie title', movie.title)
+            stars = request.data['stars']
+            #user = request.user
+            user = User.objects.get(id=1)
             
-            response = {'message': 'its working'}
-            return Response(response, status=status.HTTP_200_OK)
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            
         else:
             response = {'message': 'You need to provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)

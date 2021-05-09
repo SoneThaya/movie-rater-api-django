987b823b64fdf94a86b64d51824237a45d18be0c
sone <sthaya@gmail.com>
2021-05-09 16:03:23 -0700

    updated code from repo

--- api/serializers.py
+++ api/serializers.py
@@ -8,17 +8,17 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
-        
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
-        
+
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ('id', 'title', 'description', 'no_of_ratings', 'avg_rating')
-        
+
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
